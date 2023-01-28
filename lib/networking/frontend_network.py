e5f4781c6282617d8c7ec5bd20623982121f8ce6
syoon2 <syoon2@mta.ca>
2023-01-28 17:06:29 -0400

    socket networking

--- lib/networking/frontend_network.py
+++ lib/networking/frontend_network.py
@@ -2,7 +2,19 @@ import json, socket
 
 class ClientSocket:
 
-    def send_data():
+    __host: str
+    __port: int
+    __sock: socket.socket
+
+    def __init__(self, host: str, port: int) -> None:
+        self.__host = host
+        self.__port = port
+        self.__sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    def send_data(self, data):
+        return self.__sock.sendto(data, (self.__host, self.__port))
+
+    def receive_data(self):
         pass
 
     def parse_data(data: str):
