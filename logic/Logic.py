a63511b70f7433582420394661d3c260f057e453
BananaMcCana <95002597+BananaMcCana@users.noreply.github.com>
2023-01-28 16:58:25 -0400

    Score_move + update_hand + put_temp_move added

--- logic/Logic.py
+++ logic/Logic.py
@@ -1,31 +1,33 @@
-""" Controls all game logic.
-
-Responsible for recieving input messages from the player
-and verfiying the validity of the input. Passes the updated
-game state to the socket for transmission to the server
-Recieves game state data from the socket and updates the model
-accordingly. 
-
-Attributes:
-    board: a 217x217 list of tiles that contains the state of the
-        game board
-    hand: a list of tiles of length 6 representing the hand of the player
-    score: An integer count of the player's current score
-"""
-
 class Logic:
-    
+    """ Controls all game logic.
 
-    def verify_move(self, board, indices):
+    Responsible for recieving input messages from the player
+    and verfiying the validity of the input. Passes the updated
+    game state to the socket for transmission to the server
+    Recieves game state data from the socket and updates the model
+    accordingly. 
+
+    Attributes:
+        board: a 217x217 list of tiles that contains the state of the
+            game board
+        hand: a list of tiles of length 6 representing the hand of the player
+        score: An integer count of the player's current score
+    """
+    hand = {}
+    board ={}
+    score = 0
+
+    def verify_move(self, board, move):
         """Verifies the most recent move
 
         Ensures that a given move is legal by game rules.
 
         Args: 
-            board: contains the current game board, included the most recent
-                move to be verified.
-            indices: a list of indices of the tiles on the game board pertaining
-                to the most recent move.
+            board: contains the current game board.
+            move: A list of tuples containing a tile and it's given indices 
+                to represent the most recent move. For example:
+
+                {(Tile 1, x cord 1, y cord 1), (Tile 2, x cord 2, y cord 2)...}
         
         Returns: 
             Boolean corresponding to the validity of the move, true if the move is
@@ -49,4 +51,35 @@ class Logic:
             
         """ 
     def score_move(self, move):
+        """Scores a given move.
+
+        Args:
+            move: A list of tuples containing a tile and it's given indices 
+                to represent the most recent move. For example:
+
+                {(Tile 1, x cord 1, y cord 1), (Tile 2, x cord 2, y cord 2)...}
+
+        Returns: 
+            Integer represent of the score of the move.
+        """
+    
+    def update_hand(self, hand):
+        self.hand = hand
+    
+    def put_temp_move(self, move):
+        """Receives a move from the view to be evaluated
+
+        Args:
+            move: A list of tuples containing a tile and it's given indices 
+                to represent the most recent move. For example:
+
+                {(Tile 1, x cord 1, y cord 1), (Tile 2, x cord 2, y cord 2)...}
+
+        Returns:
+            true: if the move is valid.
+            false: if the move is invalid.
+        """
+
+
+        
 
