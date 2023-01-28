d3060567a7304b2c8b220c6cea60d4a0fe1f05fa
Benjru <ben.hallihan1@gmail.com>
2023-01-28 15:31:41 -0400

    Added basic outline of functions

---
+++ view.py
@@ -0,0 +1,33 @@
+import pygame
+
+class View:
+
+    """Renders view for frontend client
+
+    Responsible for rendering the frontend view. After initialization,
+    calls to render_grid(), render_hand(), and render_details() can be
+    performed automatically by calling update_view()
+    """
+
+    def __init__():
+        """Inits the view."""
+        pass
+
+    def update_view():
+        """Updates the entire view."""
+        pass
+
+    def render_grid():
+        """Renders the main grid on which tiles may be placed."""
+        pass
+
+    def render_hand():
+        """Renders the tiles currently held by the player."""
+        pass
+
+    def render_details():
+        """Renders details such as the server IP and the player's score."""
+        pass
+
+
+
