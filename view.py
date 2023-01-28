20203c79a403324378b056c996b0c4820813fb4c
Benjru <ben.hallihan1@gmail.com>
2023-01-28 16:57:14 -0400

    Started implementation of event loop & grid rendering

--- view.py
+++ view.py
@@ -20,22 +20,33 @@ class View:
         screen = pygame.display.set_mode(window_size)
         pygame.display.set_caption("Qwirkle")
         screen.fill(background_color)
+        self.update_view(screen, window_size)
+        self.init_event_loop()
+
+    def update_view(self, screen, window_size):
+        """Updates the entire view."""
+        self.render_grid(screen, window_size)
+        self.render_hand()
+        self.render_details()
         pygame.display.flip()
 
-    def update_view():
-        """Updates the entire view."""
-        pass
-
-    def render_grid():
+    def render_grid(self, screen, window_size):
         """Renders the main grid on which tiles may be placed."""
-        pass
+        border_color = (0, 0, 0)
+        background_color = (255, 255, 255)
+        pygame.draw.rect(screen, border_color, pygame.Rect((0.05 * window_size[0]), (0.05 * window_size[1]), (0.9 * window_size[0]), (0.8 * window_size[1])))
+		# TODO: implement hollow_rect(), replace previous line & use for grid generation
 
-    def render_hand():
+    def render_hand(self):
         """Renders the tiles currently held by the player."""
         pass
 
-    def render_details():
+    def render_details(self):
         """Renders details such as the server IP and the player's score."""
+        pass
+
+    def hollow_rect(self, x, y, width, height, border_width):
+        """Returns a hollow rectangle"""
         pass
 
     def init_event_loop(self):
@@ -46,7 +57,5 @@ class View:
                 if ev.type == pygame.QUIT:
                     sys.exit()
 
-
-# Test code - ignore
-game_view = View()
-game_view.init_event_loop()
+# Test code
+View()
