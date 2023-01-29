081ec102c56a2ed027982ca552cb660e84f0d1b5
Benjru <ben.hallihan1@gmail.com>
2023-01-29 10:45:14 -0400

    Implemented draw_hollow_rect()

--- view.py
+++ view.py
@@ -34,8 +34,7 @@ class View:
         """Renders the main grid on which tiles may be placed."""
         border_color = (0, 0, 0)
         background_color = (255, 255, 255)
-        pygame.draw.rect(screen, border_color, pygame.Rect((0.05 * window_size[0]), (0.05 * window_size[1]), (0.9 * window_size[0]), (0.8 * window_size[1])))
-		# TODO: implement hollow_rect(), replace previous line & use for grid generation
+        self.draw_hollow_rect(screen, background_color, border_color, (0.05 * window_size[0]), (0.05 * window_size[1]), (0.9 * window_size[0]), (0.8 * window_size[1]), 10)
 
     def render_hand(self):
         """Renders the tiles currently held by the player."""
@@ -45,9 +44,24 @@ class View:
         """Renders details such as the server IP and the player's score."""
         pass
 
-    def hollow_rect(self, x, y, width, height, border_width):
-        """Returns a hollow rectangle"""
-        pass
+    def draw_hollow_rect(self, screen, color, border_color, x, y, width, height, border_width):
+        """Draws a hollow rectangle
+
+            Args:
+                screen: screen object returned by pygame.display.set_mode()
+                color: the background color
+                border_color: the color of the border
+                x: x position of the top left corner
+                y: y position of the top left corner
+                width: width of the recelf.
+                height: height of the rect
+                border_width: width of the border
+
+            Returns:
+                Nothing
+        """
+        pygame.draw.rect(screen, border_color, pygame.Rect(x, y, width, height))
+        pygame.draw.rect(screen, color, pygame.Rect(x+border_width, y+border_width, width - (2 * border_width), height - (2 * border_width)))
 
     def init_event_loop(self):
         """Event loop for handling UI interaction """
