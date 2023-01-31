dd4ea559cb8e5c44a4824aff94a6342fd37c6e80
BananaMcCana <95002597+BananaMcCana@users.noreply.github.com>
2023-01-31 17:53:18 -0400

    updated gamerules

--- gamerules/gamerules.py
+++ gamerules/gamerules.py
@@ -9,7 +9,6 @@ class Gamerules:
     """
     board ={}
 
-    
     def update_board(self, board):
         self.board = board
 
@@ -25,9 +24,41 @@ class Gamerules:
                 {(Tile 1, x cord 1, y cord 1), (Tile 2, x cord 2, y cord 2)...}
         
         Returns: 
-            Boolean corresponding to the validity of the move, true if the move is
+            A boolean list corresponding to the validity of the move, true if the move is
                 determined to be legal, false if it is not.
         """
+        validPlacements = {}
+        pass
+    
+    def get_lines(self, placement):
+        """Check which lines a given placement could be a part of
+
+        Interated through the board from the placement data to see what valid lines configurations
+        could be made.
+
+        Args:
+            placement: placement data of the tile
+
+        Returns:
+            A list of possible lines in form of a list of tiles for ex:
+                {Line:(Tile: red, circle, Tile: red, square, Tile: red, star), Line:(Tile: green, square, Tile: red, square)}
+            max line length is 6
+        """
+        pass
+
+    def verify_placement(self, placement):
+        """Verifies placement of a single tile on the board
+
+        Checks to make sure a given placement is a valid move.
+
+        Args:
+            placement: placement to verify
+        Return:
+            True: if it is a valid placement
+            False: if it is not a valid placement
+        """
+        
+        pass
 
     def register_move(self, move):
         """Registers a given move.
@@ -42,13 +73,13 @@ class Gamerules:
         
         Returns:
             Boolean corresponding to if the move was succesfully registered.
-            
         """ 
+
     def score_move(self, move):
         """Scores a given move.
 
         Args:
-            move: A list of tuples containing a tile and it's given indices 
+            move: A list of placements containing a tile and it's given indices 
                 to represent the most recent move. For example:
 
                 {(Tile 1, x cord 1, y cord 1), (Tile 2, x cord 2, y cord 2)...}
