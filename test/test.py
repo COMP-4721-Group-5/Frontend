00fea9aa4cf71e571130a4895cffbc5a981264e0
syoon2 <syoon2@mta.ca>
2023-02-02 13:46:22 -0400

    Increase test coverage

--- test/test.py
+++ test/test.py
@@ -1,9 +1,21 @@
-import unittest
+import pytest
 
 from lib.shared.internal_structures import *
 
-class TileTest(unittest.TestCase):
-
-    def test_tile_hex(self):
-        red_circle = Tile(TileColor.RED, TileShape.CIRCLE)
-        self.assertEqual(red_circle.hex_value, 0x11)
+@pytest.mark.parametrize("color",
+    [
+        TileColor.RED, TileColor.ORANGE, TileColor.YELLOW,
+        TileColor.GREEN, TileColor.BLUE, TileColor.VIOLET
+    ]
+)
+@pytest.mark.parametrize("shape",
+    [
+        TileShape.CIRCLE, TileShape.CLUB, TileShape.CROSS,
+        TileShape.DIAMOND, TileShape.SQUARE, TileShape.STAR
+    ]
+)
+def test_tile_hex(color: TileColor, shape: TileShape):
+    test_tile = Tile(color, shape)
+    assert test_tile.color == color
+    assert test_tile.shape == shape
+    assert test_tile.hex_value == color.value ^ shape.value
