a9e1ddf9b85101457c6b0162ba9d7354f183c5eb
syoon2 <syoon2@mta.ca>
2023-02-04 14:11:12 -0400

    more Python OO

--- gameelements/placement.py
+++ gameelements/placement.py
@@ -1,3 +1,5 @@
+from lib.shared.internal_structures import Tile
+
 class Placement:
     """Contains placement data
 
@@ -6,13 +8,25 @@ class Placement:
         x_coord: x coordinate of the tile to be placed within the game board
         y_coord: y coordinate of the tile to be placed within the game board
     """
-    tile = None
-    x_coord = 0
-    y_coord = 0
+    __tile: Tile
+    __x_coord: int
+    __y_coord: int
 
     """Creates placement
     """
-    def __init__(self, tile, x_coord, y_coord):
-        self.tile = tile
-        self.x_coord = x_coord
-        self.y_coord = y_coord
+    def __init__(self, tile: Tile, x_coord: int, y_coord: int):
+        self.__tile = tile
+        self.__x_coord = x_coord
+        self.__y_coord = y_coord
+
+    @property
+    def tile(self):
+        return self.__tile
+    
+    @property
+    def x_coord(self):
+        return self.__x_coord
+
+    @property
+    def y_coord(self):
+        return self.__y_coord
