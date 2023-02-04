02d845cd496462b8208bc816ce4cc630d7ea879c
syoon2 <syoon2@mta.ca>
2023-02-04 14:18:25 -0400

    Move board and placement into internal_structures
    Co-authored-by: Liam Sims <jlsims@mta.ca>

--- lib/shared/internal_structures.py
+++ lib/shared/internal_structures.py
@@ -1,3 +1,4 @@
+from typing import List, Final
 from enum import IntEnum
 
 
@@ -79,3 +80,59 @@ class Tile:
             return self.__color == __o.__color and self.__shape == __o.__shape and self.__temporary == __o.__temporary
         else:
             return False
+
+class Placement:
+    """Contains placement data
+
+    Attributes:
+        tile: tile to be placed
+        x_coord: x coordinate of the tile to be placed within the game board
+        y_coord: y coordinate of the tile to be placed within the game board
+    """
+    __tile: Tile
+    __x_coord: int
+    __y_coord: int
+
+    """Creates placement
+    """
+    def __init__(self, tile: Tile, x_coord: int, y_coord: int):
+        self.__tile = tile
+        self.__x_coord = x_coord
+        self.__y_coord = y_coord
+
+    @property
+    def tile(self):
+        return self.__tile
+    
+    @property
+    def x_coord(self):
+        return self.__x_coord
+
+    @property
+    def y_coord(self):
+        return self.__y_coord
+
+class Board:
+    """Contains the representation of the gameboard
+
+    Attributes:
+        board: a 217x217 array of Tiles
+    """
+    __board: List[List[Tile]]
+    ROW: Final = 217
+    COLUMN: Final = 217
+
+    def __init__(self):
+        self.__board = [[None] * Board.COLUMN] * Board.ROW
+
+    def get_board(self) -> List[List[Tile]]:
+        return self.__board
+    
+   
+    def add_tile(self, placement: Placement):
+        """Adds tile at given coordinates
+
+        Args:
+        placement: contains (Tile, x_coord, y_coord)
+        """
+        self.board[placement.x_coord][placement.y_coord] = placement.tile
