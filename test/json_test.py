3b4813bcca0b64093359eed02259bbde593cd3d5
syoon2 <syoon2@mta.ca>
2023-02-14 16:01:07 -0400

    PyTest naming convention

--- test/json_test.py
+++ test/json_test.py
@@ -11,7 +11,7 @@ from lib.shared.internal_structures import *
     TileShape.CIRCLE, TileShape.CLUB, TileShape.CROSS, TileShape.DIAMOND,
     TileShape.SQUARE, TileShape.STAR
 ])
-def tile_json_test(color: TileColor, shape: TileShape):
+def test_tile_json(color: TileColor, shape: TileShape):
     test_tile = Tile(color, shape)
     json_form = test_tile.__json__()
     assert test_tile == Tile.load_json(json_form)
@@ -22,13 +22,13 @@ def tile_json_test(color: TileColor, shape: TileShape):
 @pytest.mark.parametrize("shape", [TileShape.CIRCLE, TileShape.CLUB])
 @pytest.mark.parametrize("x", [124, 208, 34])
 @pytest.mark.parametrize("y", [146, 12, 32])
-def placement_json_test(color: TileColor, shape: TileShape, x: int, y: int):
+def test_placement_json(color: TileColor, shape: TileShape, x: int, y: int):
     test_placement = Placement(Tile(color, shape), x, y)
     json_form = test_placement.__json__()
     assert test_placement == Placement.load_json(json_form)
 
 
-def board_json_test():
+def test_board_json():
     base_board = Board()
     base_board.get_board()[63, 64] = Tile(TileColor.RED, TileShape.DIAMOND)
     copy_board = Board()
