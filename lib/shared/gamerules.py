10874fe0de2ef29a460cd8cd64807d955b425519
BananaMcCana <95002597+BananaMcCana@users.noreply.github.com>
2023-02-14 11:19:47 -0400

    updated score_move, expanded get_lines

--- lib/shared/gamerules.py
+++ lib/shared/gamerules.py
@@ -30,8 +30,9 @@ class Gamerules:
             A boolean list corresponding to the validity of the move, true if the move is
                 determined to be legal, false if it is not.
         """
-        validPlacements = {}
         pass
+
+        
 
     def get_lines(self, placement: Placement):
         """Check which lines a given placement could be a part of
@@ -45,14 +46,14 @@ class Gamerules:
         Returns:
             A list of possible lines in form of a list of placements for ex:
                 {Line:((Tile: red, circle, x, y), (Tile: red, square, x, y), (Tile: red, star, x, y)), Line:(Tile: green, square, Tile: red, square)}
-            max line length is 6
+            max line length is 5, does not include current placement
+            returns None in both lists if there are no valid lines
         """
         x_line = list()
         y_line = list()
-        #y_line = self.__board[placement.x_coord][placement.y_coord - 6:placement.y_coord + 6]
-        #x_line = self.__board[placement.x_coord - 6:placement.x_coord + 6][placement.y_coord]
         y_count = 0
         x_count = 0
+        skip = False
         for i in range(5): #Checks up to 5 tiles above the horizontal
             if placement.y_coord + i + 1 > 217:
                 break
@@ -64,21 +65,26 @@ class Gamerules:
                 if temp_tile.shape() == placement.tile.shape() or temp_tile.color() == placement.tile.color():
                     y_line.append(temp_placement)
                     y_count += 1
-
-        for i in range(5): #Checks up to 5 tiles below the horizontal
-            if placement.y_coord - i - 1 < 0:
-                break
-            temp_tile = self.__board[placement.x_coord][placement.y_coord - i - 1]
-            temp_placement = Placement(temp_tile, placement.x_coord, placement.y_coord - i - 1)
-            if temp_tile is None:
-                break
-            else:
-                if temp_tile.shape() == placement.tile.shape() or temp_tile.color() == placement.tile.color():
-                    y_line.append(temp_placement)
-                    y_count += 1
-            if y_count > 6:
+            if temp_tile.eq(placement.tile):
                 y_line = None
-                break
+                skip = True
+
+        if skip is False: #If the y_line has a duplicate skip checking below as it is invalid placement
+            for i in range(5): #Checks up to 5 tiles below the horizontal
+                if placement.y_coord - i - 1 < 0:
+                    break
+                temp_tile = self.__board[placement.x_coord][placement.y_coord - i - 1]
+                temp_placement = Placement(temp_tile, placement.x_coord, placement.y_coord - i - 1)
+                if temp_tile is None:
+                    break
+                else:
+                    if temp_tile.shape() == placement.tile.shape() or temp_tile.color() == placement.tile.color():
+                        y_line.append(temp_placement)
+                        y_count += 1
+                #Checks if line is too long or contains duplicate tiles
+                if y_count > 5 or temp_tile.eq(placement.tile):
+                    y_line = None
+                    break
 
         #Gets the x_line
         for i in range(5): #Checks up to 5 tiles to the right of the vertical
@@ -92,6 +98,8 @@ class Gamerules:
                 if temp_tile.shape() == placement.tile.shape() or temp_tile.color() == placement.tile.color():
                     x_line.append(temp_placement)
                     x_count += 1
+                if temp_tile.eq(placement.tile): #if line already contains this tile, it is invalid
+                    return None, y_line
 
         for i in range(5): #Checks up to 5 tiles to the left of the vertical
             if placement.x_coord + i + 1 > 217:
@@ -104,11 +112,11 @@ class Gamerules:
                 if temp_tile.shape() == placement.tile.shape() or temp_tile.color() == placement.tile.color():
                     x_line.append(temp_placement)
                     x_count += 1
-            if x_count > 6:
+            #Checks if line is too large or if it already contains a given tile
+            if x_count > 5 or temp_tile.eq(placement.tile):
                 x_line = None
                 break
-
-            
+  
         return x_line, y_line
 
     def verify_placement(self, placement: Placement) -> bool:
@@ -122,8 +130,12 @@ class Gamerules:
             True: if it is a valid placement
             False: if it is not a valid placement
         """
+        x_line, y_line = self.get_lines(placement)
+        if x_line is None and y_line is None:
+            return False
+        else:
 
-        pass
+            return True
 
     def register_move(self, move) -> bool:
         """Registers a given move.
@@ -143,6 +155,8 @@ class Gamerules:
     def score_move(self, move) -> int:
         """Scores a given move.
 
+        Currently score relies on temporary tile information so scoring must happen before a move is fully
+        registered
         Args:
             move: A list of placements containing a tile and it's given indices 
                 to represent the most recent move. For example:
@@ -152,3 +166,43 @@ class Gamerules:
         Returns: 
             Integer represent of the score of the move.
         """
+        score = 0
+        for placement in move:
+            score += self.score_placement(placement)
+
+        return score
+
+    def score_placement(self, placement) -> int:
+        """Scores a given placement
+
+        Args: 
+            placement: contains the placement data in the form of (Tile, x_coord, y_coord)
+
+        Returns:
+            Integer representation of the score of the move.
+        """
+        score = 0
+        x_line, y_line = self.get_lines(placement)
+        already_scored = False #Keeps track of whether the line(s) of placement have already been scored on this turn
+        if x_line is not None:
+            for placement in x_line:
+                if placement.tile.is_temporary:
+                    already_scored = True
+            if already_scored:
+                score += 1
+            else:
+                score += len(x_line) + 1
+            if len(x_line) == 5: #Checks for Quirkle
+                score += 6
+        already_scored = False
+        if y_line is not None:
+            for placement in y_line:
+                if placement.tile.is_temporary:
+                    already_scored = True
+            if already_scored:
+                score += 1
+            else:
+                score += len(y_line)
+            if len(y_line) == 5: #Checks for Quirkle
+                score += 6
+        
