d74a6b7a25d2a9dbd87974bf66eecf96d1043eb3
syoon2 <syoon2@mta.ca>
2023-02-18 20:51:09 +0000

    Safely handle socket closing

--- lib/frontend/frontend_network.py
+++ lib/frontend/frontend_network.py
@@ -10,11 +10,13 @@ class ClientSocket:
         host: Address of the host (server)
         port: Port number to connect to
         sock: low-level socket object connected to host
+        closed: Flag indicating status of socket
     """
 
     __host: str
     __port: int
     __sock: socket.socket
+    __closed: bool
 
     def __init__(self, host: str, port: int) -> None:
         """Initializes the socket instance
@@ -27,6 +29,7 @@ class ClientSocket:
         self.__port = port
         self.__sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.__sock.connect((self.__host, self.__port))
+        self.__closed = False
 
     def send_data(self, data: str):
         """Sends given data to the connected host.
@@ -38,9 +41,22 @@ class ClientSocket:
             Response returned by the host.
         """
         self.__sock.send(data.encode())
-        return self.__sock.recv(4096).decode()
+        recv_msg = self.__sock.recv(4096)
+        if len(recv_msg) == 0:
+            self.close()
+        return recv_msg.decode()
 
     def close(self) -> None:
         """Closes connection with the server.
         """
+        if self.__closed: return
+        self.__closed = True
+        self.__sock.shutdown(socket.SHUT_WR)
+        while len(self.__sock.recv(1024)) != 0:
+            self.__sock.send(b'')
         self.__sock.close()
+    
+    @property
+    def closed(self) -> bool:
+        """Flag indicating status of socket"""
+        return self.__closed
