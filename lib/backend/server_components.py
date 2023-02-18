41302e06cdf07cb37c2153b4da9901732e594aae
syoon2 <syoon2@mta.ca>
2023-02-18 21:08:27 +0000

    Add Server-Specific Networking Components

---
+++ lib/backend/server_components.py
@@ -0,0 +1,56 @@
+from dataclasses import dataclass
+from queue import Queue
+import socket
+import time
+from threading import Event
+from threading import Thread
+from typing import Tuple, TypeAlias
+
+Address: TypeAlias = Tuple[str, int]
+
+@dataclass
+class Request:
+    connection: 'ClientConnection'
+    time: float
+    data: str
+
+class ClientConnection:
+    __csock: socket.socket
+    __addr: Address
+    __listener: 'ClientMsgListener'
+    __stop_listen: Event
+
+    def __init__(self, csock: socket.socket, addr: Address, msg_queue: Queue[Request]) -> None:
+        self.__csock = csock
+        self.__addr = addr
+        self.__listener = ClientConnection.ClientMsgListener(self, msg_queue)
+        self.__stop_listen = Event()
+    
+    def stop_listening(self) -> None:
+        self.__stop_listen.set()
+    
+
+    class ClientMsgListener(Thread):
+        __connection: 'ClientConnection'
+        __msg_queue: Queue[Request]
+
+        def __init__(self, connection: 'ClientConnection', msg_queue: Queue[Request]):
+            self.__connection = connection
+            self.__msg_queue = msg_queue
+        
+        def run(self):
+            while not self.__connection.__stop_listen.is_set():
+                recv_data = self.__connection.__csock.recv(4096).decode()
+                self.__msg_queue.put(Request(self.__connection, time.time(), recv_data))
+            
+            self.__connection.__csock.shutdown(socket.SHUT_WR)
+
+            while len(recv_data) != 0:
+                recv_data = self.__connection.__csock.recv(1024)
+            
+            self.__connection.__csock.close()
+
+
+
+
+
