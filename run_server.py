5d426ef802c4d04187e57d4359e4256eed9bb77f
syoon2 <syoon2@mta.ca>
2023-02-19 19:50:00 +0000

    Outline structure of main entrypoint

--- run_server.py
+++ run_server.py
@@ -13,8 +13,21 @@ sock.listen()
 connections: List[ClientConnection] = list()
 request_queue: Queue[Request] = Queue()
 
-for i in range(2):
+for i in range(2): # Arbitrary limitation of 2 players for POC
     csock, addr = sock.accept()
     connections.append(ClientConnection(csock, addr, request_queue))
-    
 
+
+sock.close()
+
+# TODO: Initialize game
+
+# TODO: Initialize request processor
+
+while True: # TODO: eplace with game state conditional
+    try:
+        pass
+    except KeyboardInterrupt:
+        break
+
+# TODO: Cleanup / Close
