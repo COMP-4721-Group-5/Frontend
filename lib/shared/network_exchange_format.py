9f6697a2459640b9387fa49438a9166a37b4058b
syoon2 <syoon2@mta.ca>
2023-02-24 12:26:21 -0400

    client request data format

--- lib/shared/network_exchange_format.py
+++ lib/shared/network_exchange_format.py
@@ -1,7 +1,7 @@
 from abc import ABC, abstractmethod
 from enum import IntEnum
 import json
-from typing import Any, Dict, final, TypeVar
+from typing import Any, Dict, final, List, TypeVar
 
 from .internal_structures import JsonableObject
 from .internal_structures import Tile
@@ -26,6 +26,31 @@ class JsonableDecoder(json.JSONDecoder):
                 return Placement.json_deserialize(dct)
             elif dct['type'] == 'board':
                 return Board.json_deserialize(dct)
+            elif dct['type'] == 'request':
+                return ClientRequest.json_deserialize(dct)
         else:
             return dct
+
+class ClientRequest(JsonableObject):
+    __request_type: str
+    __data: List[Tile] | List[Placement]
+
+    def __init__(self, request_type: str, data: List[Placement] | List[Tile]):
+        if request_type in ['discard', 'placement']:
+            self.__request_type = request_type
+            self.__data = data
+        else:
+            raise ValueError
+
+    def json_serialize(self) -> Dict[str, str | List[Placement] | List[Tile]]:
+        dict_form = {
+            'type': 'request',
+            'request_type': self.__request_type,
+            'data': self.__data
+        }
+        return dict_form
+    
+    def json_deserialize(serialized_form: Dict[str, str | List[Placement] | List[Tile]]):
+        new_request = ClientRequest(serialized_form['request_type'], serialized_form['data'])
+        return new_request
 
